da8e72602513219e7c5d14b7efe85cf512602b9d
SoftwareStudios <133154113+SoftwareStudios@users.noreply.github.com>
2023-06-28 10:24:05 +0700

    Create roman_numeral_converter.js
    ADD roman numeral converter algo

---
+++ roman_numeral_converter.js
@@ -0,0 +1,22 @@
+function convertToRoman(num) {
+    const romanArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const numArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romanString = "";
+
+    do {
+        for (let i = 0; i < numArray.length; i++) {
+            if (num >= numArray[i]) {
+                let count = Math.trunc(num / numArray[i]);
+                console.log("count after truncation= " + count);
+                for (let x = 0; x < count; x++) {
+                    romanString += romanArray[i];
+                }
+                num = num - (numArray[i] * count);
+                console.log(num);
+            }
+        }
+    } while (num != 0);
+    return romanString;
+}
+
+console.log(convertToRoman(12));
